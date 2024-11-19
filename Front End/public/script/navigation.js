387015f8d55f2c378e52355de685b74687cd21f5
evanalif113 <evanalifwidhyatma@gmail.com>
2024-11-19 11:06:29 +0700

    Major Update: Refactor navigation.js and index.html for improved user experience

--- Front End/public/script/navigation.js
+++ Front End/public/script/navigation.js
@@ -1,14 +1,9 @@
-// Navigasi Daftar
-document.getElementById('btnClose').addEventListener('click', function() {
-    window.location.href = './perekaman.html'; // Ganti dengan link halaman yang diinginkan
+// Navigasi Home
+document.getElementById('NavHome').addEventListener('click', function() {
+    window.location.href = './index.html'; // Ganti dengan link halaman yang diinginkan
 });
 
-//Navigasi Tentang Kami
-document.getElementById('btnClose').addEventListener('click', function() {
-    window.location.href = './perekaman.html'; // Ganti dengan link halaman yang diinginkan
-});
-
-//Navigasi Data
-document.getElementById('btnClose').addEventListener('click', function() {
+//Navigasi Perekaman
+document.getElementById('NavPerekaman').addEventListener('click', function() {
     window.location.href = './perekaman.html'; // Ganti dengan link halaman yang diinginkan
 });
