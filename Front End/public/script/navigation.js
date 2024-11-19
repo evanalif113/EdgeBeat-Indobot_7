b5419bc9d9ce6e3da271fb51d6c3c9cea5a6eee5
evanalif113 <evanalifwidhyatma@gmail.com>
2024-11-19 10:08:29 +0700

    Update index.html for WebApp Add Perekaman.html Update data.js Add Navigation.js

---
+++ Front End/public/script/navigation.js
@@ -0,0 +1,14 @@
+// Navigasi Daftar
+document.getElementById('btnClose').addEventListener('click', function() {
+    window.location.href = './perekaman.html'; // Ganti dengan link halaman yang diinginkan
+});
+
+//Navigasi Tentang Kami
+document.getElementById('btnClose').addEventListener('click', function() {
+    window.location.href = './perekaman.html'; // Ganti dengan link halaman yang diinginkan
+});
+
+//Navigasi Data
+document.getElementById('btnClose').addEventListener('click', function() {
+    window.location.href = './perekaman.html'; // Ganti dengan link halaman yang diinginkan
+});
