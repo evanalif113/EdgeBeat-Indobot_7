d660330606c082b354baaeac3305bd05d4ec0ab5
evanalif113 <evanalifwidhyatma@gmail.com>
2024-11-25 21:10:12 +0700

    Refactor perekaman.html to update board configuration for Firebase Connect

--- Firmware/Firebase_Connect/src/main.cpp
+++ Firmware/Firebase_Connect/src/main.cpp
@@ -16,29 +16,14 @@
  */
 
 #include <Arduino.h>
-#if defined(ESP32) || defined(ARDUINO_RASPBERRY_PI_PICO_W) || defined(ARDUINO_GIGA) || defined(ARDUINO_OPTA)
 #include <WiFi.h>
-#elif defined(ESP8266)
-#include <ESP8266WiFi.h>
-#elif __has_include(<WiFiNINA.h>) || defined(ARDUINO_NANO_RP2040_CONNECT)
-#include <WiFiNINA.h>
-#elif __has_include(<WiFi101.h>)
-#include <WiFi101.h>
-#elif __has_include(<WiFiS3.h>) || defined(ARDUINO_UNOWIFIR4)
-#include <WiFiS3.h>
-#elif __has_include(<WiFiC3.h>) || defined(ARDUINO_PORTENTA_C33)
-#include <WiFiC3.h>
-#elif __has_include(<WiFi.h>)
-#include <WiFi.h>
-#endif
-
 #include <FirebaseClient.h>
 
 #define WIFI_SSID "WIFI_AP"
 #define WIFI_PASSWORD "WIFI_PASSWORD"
 
 // The API key can be obtained from Firebase console > Project Overview > Project settings.
-#define API_KEY "Web_API_KEY"
+#define API_KEY ""
 
 // User Email and password that already registerd or added in your project.
 #define USER_EMAIL "USER_EMAIL"
